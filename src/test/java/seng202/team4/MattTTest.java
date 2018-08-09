3028d2daf68fbeb9cd35f6fe3cf21b89243a0632
Matthew Toohey <mct63@uclive.ac.nz>
2018-08-10 10:05:30 +1200

    Matthew Toohey Test

---
+++ src/test/java/seng202/team4/MattTTest.java
@@ -0,0 +1,13 @@
+package seng202.team4;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+public class MattTTest {
+
+    @Test
+    public void testTest(){
+        assertTrue(1+1 == 2);
+    }
+}
