14d18584c3b22bb3122d11238716aa8993e76b9d
Noel Bisson <noel.bisson@gmail.com>
2018-08-29 11:26:31 +1200

    For activity: Reordered attributes to be consistent with the databse Added constructor and getters/setters

--- src/main/java/seng202/team4/model/Activity.java
+++ src/main/java/seng202/team4/model/Activity.java
@@ -3,21 +3,36 @@ package seng202.team4.model;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Activity {
     /* The combination of name and date must be unique for a profile */
     private String name;
-    private String description;
     private LocalDate Date;
+    private String description;
+    private ActivityType type;
     private LocalTime startTime;
     private LocalTime duration;
     private double distance;
-    private double averageSpeed;
     private double caloriesBurned;
-    private ActivityType type;
-    private Collection<DataRow> rawData;
+    private double averageSpeed;
+    private List<DataRow> rawData;
 
+    public Activity(String name, LocalDate date, String description, ActivityType type, LocalTime startTime,
+                    LocalTime duration, double distance, double caloriesBurned, double averageSpeed) {
+        this.name = name;
+        Date = date;
+        this.description = description;
+        this.type = type;
+        this.startTime = startTime;
+        this.duration = duration;
+        this.distance = distance;
+        this.caloriesBurned = caloriesBurned;
+        this.averageSpeed = averageSpeed;
+        rawData = new ArrayList<>();
+    }
 
     public String getName() {
         return name;
@@ -25,5 +40,73 @@ public class Activity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return Date;
+    }
+
+    public void setDate(LocalDate date) {
+        Date = date;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getDuration() {
+        return duration;
+    }
+
+    public void setDuration(LocalTime duration) {
+        this.duration = duration;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public double getAverageSpeed() {
+        return averageSpeed;
+    }
+
+    public void setAverageSpeed(double averageSpeed) {
+        this.averageSpeed = averageSpeed;
+    }
+
+    public double getCaloriesBurned() {
+        return caloriesBurned;
+    }
+
+    public void setCaloriesBurned(double caloriesBurned) {
+        this.caloriesBurned = caloriesBurned;
+    }
+
+    public ActivityType getType() {
+        return type;
+    }
+
+    public void setType(ActivityType type) {
+        this.type = type;
+    }
+
+    public Collection<DataRow> getRawData() {
+        return rawData;
     }
 }
