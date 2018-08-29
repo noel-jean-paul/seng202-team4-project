d4e8557d3dad4c3b613d8df276a6c44f0da651fc
mjk141 <mjk141@uclive.ac.nz>
2018-08-29 16:06:56 +1200

    Implemented basic test for distance calculator

--- src/test/java/seng202/team4/Model/DataProcessorTest.java
+++ src/test/java/seng202/team4/Model/DataProcessorTest.java
@@ -1,5 +1,7 @@
 package seng202.team4.Model;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.time.LocalDate;
@@ -12,16 +14,23 @@ public class DataProcessorTest {
 
     @Test
     public void totalDistance() {
+        ArrayList<ActivityRawData> testData = new ArrayList<ActivityRawData>();
+        testData.add(new ActivityRawData(LocalDate.parse("2015-04-10"), LocalTime.parse("23:42:28"), 0, 0, 0, 0));
+        testData.add(new ActivityRawData(LocalDate.parse("2015-04-10"), LocalTime.parse("23:43:05"), 0, 0, 0, 0));
+        testData.add(new ActivityRawData(LocalDate.parse("2015-04-10"), LocalTime.parse("23:43:15"), 0, 0, 0, 0));
+        testData.add(new ActivityRawData(LocalDate.parse("2015-04-10"), LocalTime.parse("23:43:34"), 0, 0, 0, 0));
+        float distance = DataProcessor.totalDistance(testData);
+        assertEquals(155.86, distance, 0.1);
     }
 
     @Test
-    public void calculateDuration() {
-        ArrayList<ActivityRawData> testArrayList = new ArrayList<ActivityRawData>();
-        testArrayList.add(new ActivityRawData(LocalDate.parse("2015-04-10"), LocalTime.parse("23:42:28"), 0, 0, 0, 0));
-        testArrayList.add(new ActivityRawData(LocalDate.parse("2015-04-10"), LocalTime.parse("23:43:05"), 0, 0, 0, 0));
-        testArrayList.add(new ActivityRawData(LocalDate.parse("2015-04-10"), LocalTime.parse("23:43:15"), 0, 0, 0, 0));
-        testArrayList.add(new ActivityRawData(LocalDate.parse("2015-04-10"), LocalTime.parse("23:43:34"), 0, 0, 0, 0));
-        int duration = DataProcessor.calculateDuration(testArrayList);
+    public void basicDurationTest() {
+        ArrayList<ActivityRawData> testData = new ArrayList<ActivityRawData>();
+        testData.add(new ActivityRawData(LocalDate.parse("2015-04-10"), LocalTime.parse("23:42:28"), 0, 0, 0, 0));
+        testData.add(new ActivityRawData(LocalDate.parse("2015-04-10"), LocalTime.parse("23:43:05"), 0, 0, 0, 0));
+        testData.add(new ActivityRawData(LocalDate.parse("2015-04-10"), LocalTime.parse("23:43:15"), 0, 0, 0, 0));
+        testData.add(new ActivityRawData(LocalDate.parse("2015-04-10"), LocalTime.parse("23:43:34"), 0, 0, 0, 0));
+        int duration = DataProcessor.calculateDuration(testData);
         assertEquals(66, duration);
     }
 }
