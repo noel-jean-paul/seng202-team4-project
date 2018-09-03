cf08c4a2856c685d13262af665b410e0045bb50a
Noel Bisson <noel.bisson@gmail.com>
2018-09-03 15:04:40 +1200

    loadProfile is now loads full profile from the database. Helper load methods have been made private

--- src/test/java/seng202/team4/model/database/DataLoaderTest.java
+++ src/test/java/seng202/team4/model/database/DataLoaderTest.java
@@ -11,6 +11,7 @@ import seng202.team4.model.data.ProfileKey;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -33,41 +34,59 @@ public class DataLoaderTest extends DataAccesser {
         // Remove all data from the database
         DataTestHelper.clearDatabase();
 
-        // Insert test profiles
-        profile1 = new Profile("Noel", "Bisson", "1998-03-06", 85.0,
-                1.83);
-        DataStorer.insertProfile(profile1);
-
-        profile2 = new Profile("Matthew", "Michewski", "1997-06-23", 76,
-                1.85);
-        DataStorer.insertProfile(profile2);
-
-        // Insert test Activities
-        activity1 = new Activity("Run in the park", "2018-08-29", "", ActivityType.Run,
-                "12:15:01", "00:40:00", 5.13, 187);
-        DataStorer.insertActivity(activity1, profile1);
-
-        activity2 = new Activity("Walk around the block", "2018-09-01", "Quick walk",
-                ActivityType.Walk, "01:28:30", "00:11:19", 1.2, 30);
-        DataStorer.insertActivity(activity2, profile1);
-
-        // Insert test Goals
-        goal1 = new Goal(1, 55, GoalType.Walk, "2018-03-20", "2020-01-01",
-                2.00, 0);
-        DataStorer.insertGoal(goal1, profile1);
-
-        goal2 = new Goal(2, 100, GoalType.Run, "2017-05-21", "2020-01-02",
-                5.00, 60);
-        DataStorer.insertGoal(goal2, profile1);
-
-        // Insert test DataRows
+        // Create test DataRows
         row1 = new DataRow(1, "2018-07-18", "14:02:20", 182, -87.01902489,
                 178.4352, 203);
-        DataStorer.insertDataRow(row1, activity1);
+
 
         row2 = new DataRow(2, "2018-07-18", "14:02:25", 182, -87.01902489,
                 178.4352, 203);
+
+        // create test Activities
+        activity1 = new Activity("Run in the park", "2018-08-29", "", ActivityType.Run,
+                "12:15:01", "00:40:00", 5.13, 187);
+
+        activity2 = new Activity("Walk around the block", "2018-09-01", "Quick walk",
+                ActivityType.Walk, "01:28:30", "00:11:19", 1.2, 30);
+
+        // Create test Goals
+        goal1 = new Goal(1, 55, GoalType.Walk, "2018-03-20", "2020-01-01",
+                2.00, 0);
+
+        goal2 = new Goal(2, 100, GoalType.Run, "2017-05-21", "2020-01-02",
+                5.00, 60);
+
+        // Create test profiles
+        profile1 = new Profile("Noel", "Bisson", "1998-03-06", 85.0,
+                1.83);
+
+        profile2 = new Profile("Matthew", "Michewski", "1997-06-23", 76,
+                1.85);
+
+        // Add activities/ goals to profile1
+        activity1.addAllDataRows(new ArrayList<>(Arrays.asList(row2, row1)));
+        activity2.addAllDataRows(new ArrayList<>(Arrays.asList(row1, row2)));
+        profile1.addActivity(activity1);
+        profile1.addActivity(activity2);
+
+        profile1.addAllGoals(new ArrayList<>(Arrays.asList(goal1, goal2)));
+
+
+        // Insert objects
+        DataStorer.insertProfile(profile1);
+        DataStorer.insertProfile(profile2);
+
+        DataStorer.insertActivity(activity1, profile1);
+        DataStorer.insertActivity(activity2, profile1);
+        DataStorer.insertDataRow(row1, activity1);
         DataStorer.insertDataRow(row2, activity1);
+        DataStorer.insertDataRow(row1, activity2);
+        DataStorer.insertDataRow(row2, activity2);
+        DataStorer.insertGoal(goal1, profile1);
+        DataStorer.insertGoal(goal2, profile1);
+
+
+
     }
 
     @AfterClass
@@ -81,46 +100,6 @@ public class DataLoaderTest extends DataAccesser {
         assertEquals(profile1, loadedProfile);
     }
 
-    @Test
-    public void loadProfileActivities() throws SQLException {
-        // Load the list into the profile
-        DataLoader.loadProfileActivities(profile1);
-
-        // Create the expected list
-        List<Activity> expectedActivities = new ArrayList<>();
-        expectedActivities.add(activity1);
-        expectedActivities.add(activity2);
-        java.util.Collections.sort(expectedActivities);
-
-        assertEquals(profile1.getActivityList(), expectedActivities);
-    }
-
-    @Test
-    public void loadProfileGoals() throws SQLException {
-        // Load the list into the profile
-        DataLoader.loadProfileGoals(profile1);
-
-        // Create the expected list
-        List<Goal> expectedGoals = new ArrayList<>();
-        expectedGoals.add(goal2);
-        expectedGoals.add(goal1);
-        java.util.Collections.sort(expectedGoals);
-
-        assertEquals(profile1.getGoalList(), expectedGoals);
-    }
-
-    @Test
-    public void loadActivtyDataRows() throws SQLException {
-        // Load the list into the activity
-        DataLoader.loadActivityDataRows(activity1);
-
-        // Create the expected list
-        List<DataRow> expectedRows = new ArrayList<>();
-        expectedRows.add(row1);
-        expectedRows.add(row2);
-
-        assertEquals(expectedRows, activity1.getRawData());
-    }
 
     @Test
     public void fetchAllProfileKeys() throws SQLException {
