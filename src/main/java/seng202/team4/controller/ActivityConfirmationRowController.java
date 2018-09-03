7c03fb6c1904a59ea67677cbd652ada0bfa74a90
Matthew Toohey <mct63@uclive.ac.nz>
2018-09-04 00:22:25 +1200

    Added some documentation.

--- src/main/java/seng202/team4/controller/ActivityConfirmationRowController.java
+++ src/main/java/seng202/team4/controller/ActivityConfirmationRowController.java
@@ -3,24 +3,37 @@ package seng202.team4.controller;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
+/**
+ * The controller for an activityConfirmationRow.
+ */
 public class ActivityConfirmationRowController extends Controller {
 
+    /**
+     * Constructor of the ActivityConfirmationRow.
+     *
+     * @param applicationStateManager The ApplicationStateManager of the application.
+     */
     public ActivityConfirmationRowController(ApplicationStateManager applicationStateManager) {
         super(applicationStateManager);
     }
 
+    /** The Text that displays the date of the Activity. */
     @FXML
     private Text activityDate;
 
+    /** The Text that displays the distance of the Activity. */
     @FXML
     private Text activityDistance;
 
+    /** The Text that displays the name of the Activity. */
     @FXML
     private Text activityName;
 
+    /** The Text that displays the duration of the Activity. */
     @FXML
     private Text activityDuration;
 
+    /** The Text that displays the type of the Activity, */
     @FXML
     private Text activityType;
 
@@ -29,17 +42,21 @@ public class ActivityConfirmationRowController extends Controller {
         activityDate.setText(text);
     }
 
+
     public void setActivityDistanceText(String text) {
         activityDistance.setText(text);
     }
+
 
     public void setActivityNameText(String text) {
         activityName.setText(text);
     }
 
+
     public void setActivityDurationText(String text) {
         activityDuration.setText(text);
     }
+
 
     public void setActivityTypeText(String text) {
         activityType.setText(text);
--- src/main/java/seng202/team4/controller/ActivityTabController.java
+++ src/main/java/seng202/team4/controller/ActivityTabController.java
@@ -4,13 +4,25 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
 import seng202.team4.Utilities;
 
+/**
+ * Controller for the Activity Tab.
+ */
 public class ActivityTabController extends Controller {
 
+    /**
+     * Constructor for the ActivityTabController.
+     *
+     * @param applicationStateManager The ApplicationStateManager of the application.
+     */
     public ActivityTabController(ApplicationStateManager applicationStateManager) {
         super(applicationStateManager);
     }
 
 
+    /**
+     * The method that is called when the user clicks the add activity button.
+     * Displays a prompt to the user on how they would like to add activities.
+     */
     @FXML
     public void addActivities() {
         Pane popUp = Utilities.loadPane("ActivityImportTypePrompt.fxml", new ActivityImportTypePromptController(applicationStateManager));
