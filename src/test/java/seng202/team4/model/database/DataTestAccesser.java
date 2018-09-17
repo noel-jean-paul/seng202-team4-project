8c73d135edfc60efc9bfdddb3405cde487120b7c
Noel Bisson <noel.bisson@gmail.com>
2018-09-17 16:12:02 +1200

    Added javadoc

--- src/test/java/seng202/team4/model/database/DataTestAccesser.java
+++ src/test/java/seng202/team4/model/database/DataTestAccesser.java
@@ -4,6 +4,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 abstract public class DataTestAccesser extends DataAccesser {
+    /**
+     * Initialise the connection to the test database
+     * @throws SQLException if the database connection fails
+     */
     public static void initialiseConnection() throws SQLException {
         String url = "jdbc:sqlite:testDatabase.sqlite";
         connection = DriverManager.getConnection(url);
