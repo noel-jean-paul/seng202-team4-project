4783a31748b7664c3cb8a624b8bfa48a8bfbb0f5
mjk141 <mjk141@uclive.ac.nz>
2018-09-18 10:54:07 +1200

    Started the framework of HealthWarning

---
+++ src/main/java/seng202/team4/model/utilities/HealthWarning.java
@@ -0,0 +1,10 @@
+package seng202.team4.model.utilities;
+
+import seng202.team4.model.utilities.enums.WarningType;
+
+public class HealthWarning {
+    WarningType type;
+    String warningURL;
+
+
+}
