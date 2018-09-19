ad4fa2ac67e0ab3c61af8b094fce6ea2c94099e5
Noel Bisson <noel.bisson@gmail.com>
2018-09-19 14:08:09 +1200

    Actual commit - updated import controller to store datarows in the database

--- src/main/java/seng202/team4/testfile.java
+++ src/main/java/seng202/team4/testfile.java
@@ -1,5 +1,6 @@
 package seng202.team4;
 
+import javafx.scene.chart.XYChart;
 import seng202.team4.model.data.Profile;
 import seng202.team4.model.data.enums.ActivityType;
 import seng202.team4.model.data.enums.GoalType;
@@ -11,13 +12,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.Duration;
 
-public class testfile  {
-    public static void main(String[] args) throws SQLException {
-        DataAccesser.initialiseConnection();
-        DataAccesser.clearDatabase();
-
-        Profile profile1 = new Profile("Noel", "Bisson", "1998-03-06", 299,
-                1.83);
-        DataStorer.insertProfile(profile1);
-    }
+public class testfile {
 }
+
+
+
