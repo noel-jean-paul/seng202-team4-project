d613e8029e711dff260f4c7cb55718bbbf40d3cc
mjk141 <mjk141@uclive.ac.nz>
2018-09-21 00:59:10 +1200

    Created setUpURL method and included javadoc templates

--- src/main/java/seng202/team4/model/utilities/HealthWarning.java
+++ src/main/java/seng202/team4/model/utilities/HealthWarning.java
@@ -5,16 +5,18 @@ import seng202.team4.model.data.Profile;
 import seng202.team4.model.data.enums.ActivityType;
 import seng202.team4.model.data.enums.WarningType;
 
-import java.time.Duration;
+import java.time.LocalDate;
 
 public class HealthWarning {
     private int avgHeartRate;
     private int minHeartRate;
     private int maxHeartRate;
     private String description;
+    private String url;
     private Activity activity;
     private Profile user;
     private WarningType type;
+    private LocalDate warningDate;
     private boolean healthRisk;
 
     public HealthWarning(Activity activity, Profile user, WarningType type, int avgHearRate, int minHeartRate, int maxHeartRate) {
@@ -24,9 +26,16 @@ public class HealthWarning {
         this.minHeartRate = minHeartRate;
         this.maxHeartRate = maxHeartRate;
         this.type = type;
+        this.warningDate = activity.getDate();
         this.healthRisk = checkRisk();
+        this.url = setUpURL();
     }
 
+    // TODO JavaDoc - Kenny
+
+    /**
+     * @return
+     */
     private boolean checkRisk() {
         boolean atRisk;
         if (activity.getType() == ActivityType.Run) {
@@ -43,28 +52,44 @@ public class HealthWarning {
         return atRisk;
     }
 
+    // TODO - Create modular approach to description setting
+
+    /**
+     * @return
+     */
     private boolean runningRisk() {
         boolean atRisk = false;
         if (type == WarningType.Tachy) {
             atRisk = tachyRisk();
-        } else if (type == WarningType.Brachy) {
-            atRisk = brachyRisk();
+            description = "Heart rate in excess of recommended maximum.";
+        } else if (type == WarningType.Brady) {
+            atRisk = bradyRisk();
+            description = "Heart rate under recommended minimum.";
         }
         return atRisk;
     }
 
+    /**
+     * @return
+     */
     private boolean walkingRisk() {
         boolean atRisk;
         if (type == WarningType.Tachy) {
             atRisk = tachyRisk();
-        } else if (type == WarningType.Brachy) {
-            atRisk = brachyRisk();
+            description = "Heart rate in excess of recommended maximum.";
+        } else if (type == WarningType.Brady) {
+            atRisk = bradyRisk();
+            description = "Heart rate under recommended minimum.";
         } else {
             atRisk = cardioRisk();
+            description = "Resting heart rate in excess of recommend maximum";
         }
         return atRisk;
     }
 
+    /**
+     * @return
+     */
     private boolean tachyRisk() {
         if (maxHeartRate > (220 - user.getAge())) {
             return true;
@@ -73,7 +98,10 @@ public class HealthWarning {
         }
     }
 
-    private boolean brachyRisk() {
+    /**
+     * @return
+     */
+    private boolean bradyRisk() {
         if (user.getAge() >= 18 && minHeartRate < 50) {
             return true;
         } else if (user.getAge() < 18 && minHeartRate < 60) {
@@ -83,6 +111,9 @@ public class HealthWarning {
         }
     }
 
+    /**
+     * @return
+     */
     private boolean cardioRisk() {
         if (minHeartRate > 83) {
             return true;
@@ -91,33 +122,76 @@ public class HealthWarning {
         }
     }
 
+    /**
+     * @return
+     */
+    private String setUpURL() {
+        String warningURL;
+        if (type == WarningType.Brady) {
+            warningURL = "https://www.google.com/search?q=tachycardia&ie=utf-8";
+        } else if (type == WarningType.Tachy) {
+            warningURL = "https://www.google.com/search?q=bradycardia&ie=utf-8";
+        } else {
+            warningURL = "https://www.google.com/search?q=cardiovascular+mortality&ie=utf-8";
+        }
+        return warningURL;
+    }
+
+    /**
+     * @return
+     */
     public boolean isHealthRisk() {
         return healthRisk;
     }
 
+    /**
+     * @return
+     */
     public int getAvgHeartRate() {
         return avgHeartRate;
     }
 
+    /**
+     * @return
+     */
     public String getAvgHeartRateString() {
         return Integer.toString(avgHeartRate);
     }
 
+    /**
+     * @return
+     */
     public int getMinHeartRate() {
         return avgHeartRate;
     }
 
+    /**
+     * @return
+     */
     public String getMinHeartRateString() {
         return Integer.toString(minHeartRate);
     }
 
+    /**
+     * @return
+     */
     public int getMaxHeartRate() {
         return maxHeartRate;
     }
 
+    /**
+     * @return
+     */
     public String getMaxHeartRateString() {
         return Integer.toString(maxHeartRate);
     }
 
 
+    public LocalDate getWarningDate() {
+        return warningDate;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
