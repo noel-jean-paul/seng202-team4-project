5b64a0fcc69b353302ef1d8b848eb3c69a693435
Matthew Toohey <mct63@uclive.ac.nz>
2018-09-21 01:02:26 +1200

    Merge branch 'master' of https://eng-git.canterbury.ac.nz/nbi21/Seng202_Team4
    # Conflicts:
    #	fitness_tracker.sqlite

--- src/main/java/seng202/team4/controller/ProfileScreenController.java
+++ src/main/java/seng202/team4/controller/ProfileScreenController.java
@@ -8,6 +8,8 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import seng202.team4.model.data.Profile;
 
+import java.time.LocalDate;
+
 public class ProfileScreenController extends Controller {
 
     @FXML
@@ -21,6 +23,15 @@ public class ProfileScreenController extends Controller {
 
     @FXML
     private Text weightText;
+
+    @FXML
+    private Text yearText;
+
+    @FXML
+    private Text monthText;
+
+    @FXML
+    private Text dayText;
 
     @FXML
     private Text errorText;
@@ -38,12 +49,24 @@ public class ProfileScreenController extends Controller {
     private VBox weightVbox;
 
     @FXML
+    private VBox dayVbox;
+
+    @FXML
+    private VBox monthVbox;
+
+    @FXML
+    private VBox yearVbox;
+
+    @FXML
     private Button editCancelButton;
 
     private TextField firstNameTextField;
     private TextField lastNameTextField;
     private TextField heightTextField;
     private TextField weightTextField;
+    private TextField dayTextField;
+    private TextField monthTextField;
+    private TextField yearTextField;
 
     private boolean isEditing = false;
 
@@ -57,11 +80,17 @@ public class ProfileScreenController extends Controller {
         lastNameTextField = new TextField();
         heightTextField = new TextField();
         weightTextField = new TextField();
+        dayTextField = new TextField();
+        monthTextField = new TextField();
+        yearTextField = new TextField();
 
         firstNameTextField.setAlignment(Pos.CENTER);
         lastNameTextField.setAlignment(Pos.CENTER);
         heightTextField.setAlignment(Pos.CENTER);
         weightTextField.setAlignment(Pos.CENTER);
+        dayTextField.setAlignment(Pos.CENTER);
+        monthTextField.setAlignment(Pos.CENTER);
+        yearTextField.setAlignment(Pos.CENTER);
     }
 
     @FXML
@@ -71,6 +100,22 @@ public class ProfileScreenController extends Controller {
             String lastName = lastNameTextField.getText();
             String weightString = weightTextField.getText();
             String heightString = heightTextField.getText();
+
+            String dayString = dayTextField.getText();
+            String monthString = monthTextField.getText();
+            String yearString = yearTextField.getText();
+
+            String dateString = String.format("%4s-%2s-%2s", yearString, monthString, dayString).replace(' ', '0');
+
+            // Try to parse the date string to check that it is in a valid format.
+            LocalDate dateOfBirth = null;
+            boolean isValidDateFormat = false;
+            try {
+                dateOfBirth = LocalDate.parse(dateString);
+                isValidDateFormat = true;
+            } catch (Exception e) {
+                isValidDateFormat = false;
+            }
 
             double weight = 0.0;
             boolean isValidWeightDoubleString = false;
@@ -96,11 +141,11 @@ public class ProfileScreenController extends Controller {
                 errorText.setText(String.format("First name must be between %s and %s characters.", Profile.MIN_NAME_SIZE, Profile.MAX_NAME_SIZE));
             } else if (!Profile.isValidName(lastName)){
                 errorText.setText(String.format("Last name must be between %s and %s characters.", Profile.MIN_NAME_SIZE, Profile.MAX_NAME_SIZE));
-            } /*else if (!isValidDateFormat) {
+            } else if (!isValidDateFormat) {
                 errorText.setText("Date should be in the form dd/mm/yyyy.");
             } else if (!Profile.isValidDateOfBirth(dateOfBirth)) {
                 errorText.setText(String.format("Date should be between %s and %s.", Profile.MIN_DOB, LocalDate.now()));
-            } */else if (!isValidWeightDoubleString || !Profile.isValidWeight(weight)) {
+            } else if (!isValidWeightDoubleString || !Profile.isValidWeight(weight)) {
                 errorText.setText(String.format("Weight must be a number between %s and %s", 0, Profile.MAX_WEIGHT));
             } else if (!isValidHeightDoubleString || !Profile.isValidHeight(height)) {
                 errorText.setText(String.format("Height must be a number between %s and %s", 0, Profile.MAX_HEIGHT));
@@ -112,17 +157,18 @@ public class ProfileScreenController extends Controller {
                     profile.setLastName(lastName);
                     profile.setWeight(weight);
                     profile.setHeight(height);
+                    profile.setDateOfBirth(dateString);
+
+                    updateInformation();
+                    changeValuesToText();
+                    editCancelButton.setText("Edit");
+                    isEditing = false;
+                    errorText.setText("");
                 } catch (java.sql.SQLException e) {
                     applicationStateManager.displayErrorMessage("Error encountered editing profile.", e.getMessage());
                 }
             }
 
-            updateInformation();
-            changeValuesToText();
-
-            editCancelButton.setText("Edit");
-            isEditing = false;
-            errorText.setText("");
         } else {
             applicationStateManager.switchToScreen("MainScreen");
         }
@@ -134,11 +180,17 @@ public class ProfileScreenController extends Controller {
             lastNameTextField.setText(applicationStateManager.getCurrentProfile().getLastName());
             heightTextField.setText(Double.toString(applicationStateManager.getCurrentProfile().getHeight()));
             weightTextField.setText(Double.toString(applicationStateManager.getCurrentProfile().getWeight()));
+            dayTextField.setText(Integer.toString(applicationStateManager.getCurrentProfile().getDateOfBirth().getDayOfMonth()));
+            monthTextField.setText(Integer.toString(applicationStateManager.getCurrentProfile().getDateOfBirth().getMonthValue()));
+            yearTextField.setText(Integer.toString(applicationStateManager.getCurrentProfile().getDateOfBirth().getYear()));
 
             firstNameVbox.getChildren().setAll(firstNameTextField);
             lastNameVbox.getChildren().setAll(lastNameTextField);
             heightVbox.getChildren().setAll(heightTextField);
             weightVbox.getChildren().setAll(weightTextField);
+            dayVbox.getChildren().setAll(dayTextField);
+            monthVbox.getChildren().setAll(monthTextField);
+            yearVbox.getChildren().setAll(yearTextField);
 
             editCancelButton.setText("Cancel");
             isEditing = true;
@@ -157,6 +209,9 @@ public class ProfileScreenController extends Controller {
         lastNameText.setText(applicationStateManager.getCurrentProfile().getLastName());
         heightText.setText(Double.toString(applicationStateManager.getCurrentProfile().getHeight()));
         weightText.setText(Double.toString(applicationStateManager.getCurrentProfile().getWeight()));
+        dayText.setText(Integer.toString(applicationStateManager.getCurrentProfile().getDateOfBirth().getDayOfMonth()));
+        monthText.setText(Integer.toString(applicationStateManager.getCurrentProfile().getDateOfBirth().getMonthValue()));
+        yearText.setText(Integer.toString(applicationStateManager.getCurrentProfile().getDateOfBirth().getYear()));
     }
 
     private void changeValuesToText() {
@@ -164,6 +219,9 @@ public class ProfileScreenController extends Controller {
         lastNameVbox.getChildren().setAll(lastNameText);
         heightVbox.getChildren().setAll(heightText);
         weightVbox.getChildren().setAll(weightText);
+        dayVbox.getChildren().setAll(dayText);
+        monthVbox.getChildren().setAll(monthText);
+        yearVbox.getChildren().setAll(yearText);
     }
 
 
