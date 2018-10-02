3c65cb3473ad49920bc97c244283fe39f583aaa9
Matthew Toohey <mct63@uclive.ac.nz>
2018-10-03 12:49:40 +1300

    added method to reset the goal tab.

--- src/main/java/seng202/team4/controller/GoalsTabController.java
+++ src/main/java/seng202/team4/controller/GoalsTabController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import seng202.team4.model.data.Goal;
 
 
 /**
@@ -13,26 +14,47 @@ import javafx.scene.text.Text;
  */
 public class GoalsTabController extends Controller {
 
+    /** Button for toggling calendar view. */
     @FXML
     private Button calendarViewButton;
 
+    /** Button for editing the selected goal. */
+    @FXML
+    private Button editButton;
+
+    /** Text for displaying the start date of the goal. */
     @FXML
     private Text startDateText;
 
+    /** Text for displaying the time remaining for the goal. */
     @FXML
     private Text remainingTimeText;
 
+    /** The ProgressIndicator that displays the progress of the selected goal. */
     @FXML
     private ProgressIndicator goalProgressIndicator;
 
+    /** Text for displaying the description of the goal. */
     @FXML
     private Text descriptionText;
 
+    /** Text for displaying the current amount of the goal that is complete. */
+    @FXML
+    private Text currentAmountText;
+
+    /** Text for displaying the total amount needed for the goal to be completed. */
+    @FXML Text totalAmountText;
+
+    /** The VBost that holds a list of the users goals. */
     @FXML
     private VBox goalsListVbox;
 
+    /** Text for displaying the expiry date of the goal. */
     @FXML
     private Text expiryDateText;
+
+    /** The currently selected goal. */
+    private Goal selectedGoal = null;
 
 
     /**
@@ -48,7 +70,7 @@ public class GoalsTabController extends Controller {
     /** Initializes the goals tab. */
     @FXML
     public void initialize() {
-
+        reset();
     }
 
     @FXML
@@ -63,6 +85,20 @@ public class GoalsTabController extends Controller {
 
     @FXML
     void edit() {
+
+    }
+
+    public void reset() {
+        selectedGoal = null;
+        goalProgressIndicator.setProgress(0);
+        goalProgressIndicator.setDisable(true);
+        editButton.setDisable(true);
+        descriptionText.setText("");
+        startDateText.setText("");
+        expiryDateText.setText("");
+        remainingTimeText.setText("");
+        currentAmountText.setText("");
+        totalAmountText.setText("");
 
     }
 }
