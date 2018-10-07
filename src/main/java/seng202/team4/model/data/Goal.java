6217e01c650065d182c0c42eae07a090af39e1be
Noel Bisson <noel.bisson@gmail.com>
2018-10-07 13:55:12 +1300

    Removed goal distance

--- src/main/java/seng202/team4/model/data/Goal.java
+++ src/main/java/seng202/team4/model/data/Goal.java
@@ -41,7 +41,6 @@ public class Goal implements Comparable<Goal> {
     private LocalDate creationDate;
     private LocalDate expiryDate;
     private LocalDate completionDate;
-    private String description;
     private double goalDistance;    // total distance of the goal in kms
     private Duration goalDuration;
     private int caloriesBurned;
@@ -202,12 +201,12 @@ public class Goal implements Comparable<Goal> {
         this.caloriesBurned = caloriesBurned;
     }
 
+    /** Get the description of the goal (regenerates using generateDescription)
+     *
+     * @return the description of the goal
+     */
     public String getDescription() {
         return generateDescription();
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public double getGoalDistance() {
