79b4f4f1de560ea94052da310a2babd163279929
mjk141 <mjk141@uclive.ac.nz>
2018-10-08 03:23:21 +1300

    Javadoc'ed my files, removed the redundant detector class and changed the type string for health warnings to use the toString method for warningType

--- src/main/java/seng202/team4/model/data/enums/WarningType.java
+++ src/main/java/seng202/team4/model/data/enums/WarningType.java
@@ -1,9 +1,14 @@
 package seng202.team4.model.data.enums;
 
+/** Enum values for the different health warning types */
 public enum WarningType {
     Brady, Tachy;
 
     @Override
+    /**
+     * New toString method for when the string Representation of the warning type is needed.
+     * @return the string representation of the enum value.
+     */
     public String toString() {
         switch(this) {
             case Brady: return "Bradycardia";
